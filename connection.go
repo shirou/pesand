00b4cb891fd97b424358c5832cc93f2f843495d8
Shirou WAKAYAMA <shirou.faw@gmail.com>
2015-09-23 09:00:38 +0900

    add StoredQueue which store msgids in the memory.

--- connection.go
+++ connection.go
@@ -3,10 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	log "github.com/Sirupsen/logrus"
-	proto "github.com/huin/mqtt"
 	"net"
 	"time"
+
+	log "github.com/Sirupsen/logrus"
+	proto "github.com/huin/mqtt"
 )
 
 // ConnectionErrors is an array of errors corresponding to the
@@ -468,4 +469,43 @@ func NewConnection(b *Broker, conn net.Conn) *Connection {
 		},
 	}
 	return c
+}
+
+// StoredQueue is a basic FIFO queue based on a circular list that resizes as needed.
+type StoredQueue struct {
+	nodes []string
+	size  int
+	head  int
+	tail  int
+	count int
+}
+
+func NewStoredQueue() *StoredQueue {
+	return &StoredQueue{}
+}
+
+// Push adds a node to the queue.
+func (q *StoredQueue) Push(n string) {
+	if q.head == q.tail && q.count > 0 {
+		nodes := make([]string, len(q.nodes)+q.size)
+		copy(nodes, q.nodes[q.head:])
+		copy(nodes[len(q.nodes)-q.head:], q.nodes[:q.head])
+		q.head = 0
+		q.tail = len(q.nodes)
+		q.nodes = nodes
+	}
+	q.nodes[q.tail] = n
+	q.tail = (q.tail + 1) % len(q.nodes)
+	q.count++
+}
+
+// Pop removes and returns a node from the queue in first to last order.
+func (q *StoredQueue) Pop() string {
+	if q.count == 0 {
+		return ""
+	}
+	node := q.nodes[q.head]
+	q.head = (q.head + 1) % len(q.nodes)
+	q.count--
+	return node
 }
